c76eccc04f7ecc878cd4a370dd4b7f46c72dcf2e
Erik Scow <erikscow@gmail.com>
2020-02-20 19:17:32 -0800

    Created the bsse, unstyled form using formik.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import formik from 'formik'
-import yup from 'yup'
-import axios from 'axios'
-
-import Form from './Form'
+import UserForm from './Form'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserForm />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,46 @@
 import React from 'react'
 
-const Form = props => {
+import { withFormik, Form, Field } from 'formik'
+import formik from 'formik'
+import yup from 'yup'
+import axios from 'axios'
+
+const UserForm = props => {
     console.log(props)
     return(
-        <div></div>
+        <div>
+            <Form>
+                <label>
+                    Name:
+                    <Field type='text' name='name' placeholder='name'/>
+                </label>
+                <label>
+                    Email:
+                    <Field type='text' name='email' placeholder='email'/>
+                </label>
+                <label>
+                    Password:
+                    <Field type='password' name='password'/>
+                </label>
+                <label>
+                    Have you read and agreed to the terms of service?:
+                    <Field type='checkbox' name='tos'></Field>
+                </label>
+                <button type="submit">Submit</button>
+            </Form>
+        </div>
     )
 }
 
-export default Form
+export default withFormik({
+    mapPropsToValues: () => ({
+        name: '',
+        email: '',
+        password: '',
+        tos: ''
+    }),
+
+    handleSubmit(values){
+        console.log(values)
+    }
+})(UserForm)
