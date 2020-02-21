49aeab635a28bb17bd091619a15d84010349797b
Erik Scow <erikscow@gmail.com>
2020-02-21 11:36:07 -0800

    Added validation using yup. Error messages display when user leaves blank after touching.

--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,40 @@
 import React from 'react'
 
-import { withFormik, Form, Field } from 'formik'
-import formik from 'formik'
-import yup from 'yup'
+import { withFormik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import axios from 'axios'
 
 const UserForm = props => {
-    console.log(props)
     return(
         <div>
             <Form>
                 <label>
                     Name:
                     <Field type='text' name='name' placeholder='name'/>
+                    {props.touched.name && props.errors.name && (
+                        <p>{props.errors.name}</p>
+                    )}
                 </label>
                 <label>
                     Email:
                     <Field type='text' name='email' placeholder='email'/>
+                    {props.touched.email && props.errors.email && (
+                        <p>{props.errors.email}</p>
+                    )}
                 </label>
                 <label>
                     Password:
                     <Field type='password' name='password'/>
+                    {props.touched.password && props.errors.password && (
+                        <p>{props.errors.password}</p>
+                    )}
                 </label>
                 <label>
                     Have you read and agreed to the terms of service?:
                     <Field type='checkbox' name='tos'></Field>
+                    {props.touched.tos && props.errors.tos && (
+                        <p>{props.errors.tos}</p>
+                    )}
                 </label>
                 <button type="submit">Submit</button>
             </Form>
@@ -37,7 +47,23 @@ export default withFormik({
         name: '',
         email: '',
         password: '',
-        tos: ''
+        tos: false
+    }),
+
+    validationSchema: Yup.object().shape({
+        name: Yup
+            .string('Invalid')
+            .required('Name is a required field'),
+        email: Yup
+            .string().email('Invalid Email')
+            .required('Email is a required field'),
+        password: Yup
+            .mixed(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character")
+            .required('Password is a required field'),
+        tos: Yup
+            .boolean()
+            .oneOf([true], 'Please read and agree to the terms of service')
     }),
 
     handleSubmit(values){
